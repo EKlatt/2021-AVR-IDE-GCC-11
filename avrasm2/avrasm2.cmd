3e23c4820de752f83294d6d85cf007d738435e1e
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2021-03-10 12:31:20 +0100

    Update avrasm2.cmd

--- avrasm2/avrasm2.cmd
+++ avrasm2/avrasm2.cmd
@@ -4,7 +4,8 @@
 @ECHO Link: https://www.microchip.com/en-us/development-tools-tools-and-software/microchip-studio-for-avr-and-sam-devices
 @ECHO Download: https://www.microchip.com/content/dam/mchp/documents/parked-documents/as-installer-7.0.2542-full.exe
 @ECHO Install Atmal Studio 7.0
-@ECHO Look for <Your Path>\Atmel\Studio\7.0\toolchain\avr8\avrassembler\avrasm2.exe
-@ECHO Copy avrasm2.exe to <your path>\MyIDE\avrasm2\avrasm2.exe
+@ECHO Look for "Your Path"\Atmel\Studio\7.0\toolchain\avr8\avrassembler\avrasm2.exe
+@ECHO Copy avrasm2.exe to "your path"\MyIDE\avrasm2\avrasm2.exe
+@ECHO ...
 @ECHO ...
 @ECHO --------------------------------------------------------------------------------
