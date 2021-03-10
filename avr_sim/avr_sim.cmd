912027b200bb78423ec444e4412b43c612cfb5d3
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2021-03-10 12:01:04 +0100

    Create avr_sim.cmd

---
+++ avr_sim/avr_sim.cmd
@@ -0,0 +1,12 @@
+@ECHO --------------------------------------------------------------------------------
+@ECHO Download release in order to get executable file...
+@ECHO I prefer Gerd's AVR Simulator
+@ECHO Link: http://www.avr-asm-tutorial.net/avr_sim/index_de.html
+@ECHO Download: http://www.avr-asm-tutorial.net/avr_sim/23/avr_sim_23_win64_debug.zip
+@ECHO Copy avr_sim.exe to <your path>\MyIDE\avr_sim\avr_sim.exe
+@ECHO ...
+@ECHO GNU-alternative
+@ECHO GNU AVR Simulator
+@ECHO Link: http://avr.sourceforge.net/#intoduction
+@ECHO Download: https://sourceforge.net/projects/avr/files/latest/download
+@ECHO --------------------------------------------------------------------------------
