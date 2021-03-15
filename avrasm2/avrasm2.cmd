4c29e6f7cbe78a0dd3ece82cea94af84f877c0d9
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2021-03-15 18:19:49 +0100

    Delete avrasm2.cmd

--- avrasm2/avrasm2.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO --------------------------------------------------------------------------------
-@ECHO Download release in order to get executable file...
-@ECHO We need: avrasm2.exe
-@ECHO Link: https://www.microchip.com/en-us/development-tools-tools-and-software/microchip-studio-for-avr-and-sam-devices
-@ECHO Download: https://www.microchip.com/content/dam/mchp/documents/parked-documents/as-installer-7.0.2542-full.exe
-@ECHO Install Atmal Studio 7.0
-@ECHO Look for "Your Path"\Atmel\Studio\7.0\toolchain\avr8\avrassembler\avrasm2.exe
-@ECHO Copy avrasm2.exe to "your path"\MyIDE\avrasm2\avrasm2.exe
-@ECHO --------------------------------------------------------------------------------
