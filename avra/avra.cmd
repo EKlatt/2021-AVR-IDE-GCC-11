177c95654802b7e9ce7d0e956472fa911c0ebe56
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2021-03-16 14:26:54 +0100

    Create avra.cmd

---
+++ avra/avra.cmd
@@ -0,0 +1,8 @@
+@ECHO --------------------------------------------------------------------------------
+@ECHO Download avra release in order to compile executable file
+@ECHO We need: avra.exe
+@ECHO 1. Download: https://github.com/Ro5bert/avra/archive/1.4.2.zip
+@ECHO 2. Instructions: https://rclelectronics.blogspot.com/2014/12/avra-assembler-under-windows.html
+@ECHO 3. Compile source with e.g. "Dev-C++"
+@ECHO 4. Copy avra.exe to "your path"\MyIDE\avra\avra.exe
+@ECHO --------------------------------------------------------------------------------
