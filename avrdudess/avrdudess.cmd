e08cfb6af9b518098ad821169ac2516b85366a82
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2021-05-20 12:12:37 +0200

    Create avrdudess.cmd

---
+++ avrdudess/avrdudess.cmd
@@ -0,0 +1,8 @@
+@ECHO --------------------------------------------------------------------------------
+@ECHO Download release in order to get executable file...
+@ECHO Zak Kemble's gui for avrdude
+@ECHO Link: https://blog.zakkemble.net/avrdudess-a-gui-for-avrdude/
+@ECHO Download: https://blog.zakkemble.net/download/AVRDUDESS-2.13-portable.zip
+@ECHO Copy contents of "AVRDUDESS-2.13-portable" to "your path"\MyIDE\avrdudess
+@ECHO ...
+@ECHO --------------------------------------------------------------------------------
